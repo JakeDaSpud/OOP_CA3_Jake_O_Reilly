111aa57e876de7bd404b02297e7ef9c5bef64efc
Jake O'Reilly <78006039+JakeDaSpud@users.noreply.github.com>
2024-02-10 19:00:53 +0000

    - Started q9 this seems good, i can cook here i see it

--- src/CA3_Question9.java
+++ src/CA3_Question9.java
@@ -10,6 +10,33 @@ enum DIRECTION {NORTH, SOUTH,EAST,WEST};
 
 public class CA3_Question9
 {
+    //basically a pair of x and y coords
+    static class Player {
+        private int x;
+        private int y;
+
+        public Player(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public int getX() {
+            return x;
+        }
+
+        public int getY() {
+            return y;
+        }
+
+        public void setX(int x) {
+            this.x = x;
+        }
+
+        public void setY(int y) {
+            this.y = y;
+        }
+    }
+
     public static void display(int[][] image)
     {
         for (int x = 0; x < image.length; x++)
@@ -21,8 +48,17 @@ public class CA3_Question9
             System.out.println();
         }
     }
-    public void solve(int x, int y, DIRECTION dir)
-    {
 
+    public void solve(int x, int y, DIRECTION dir) {
+        Player player = new Player(1, 1);
+        //while ()
+    }
+
+    public void setupMaze(int[][] maze) {
+
+    }
+
+    public static void main(String[] args) {
+        display(new int[7][7]);
     }
 }
