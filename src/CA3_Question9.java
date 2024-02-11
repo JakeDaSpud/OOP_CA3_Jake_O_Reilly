e311eee4ab2b8219cac7df4b6f30ed31d2b45c79
Jake O'Reilly <78006039+JakeDaSpud@users.noreply.github.com>
2024-02-11 21:48:30 +0000

    - Q9 valid player check works now
    - Swapped all maze[][] player checks, as it is maze[y][x] instead of maze[x][y] like i'd expected...

--- src/CA3_Question9.java
+++ src/CA3_Question9.java
@@ -1,3 +1,7 @@
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Stack;
+
 /**
  *  Name: jake o'reilly
  *  Class Group: gd2a
@@ -36,9 +40,43 @@ public class CA3_Question9
             this.y = y;
         }
     }
+    static class XYDPair {
+        private int x;
+        private int y;
+        private DIRECTION direction = DIRECTION.NORTH;
 
-    public static void display(int[][] image)
-    {
+        public XYDPair(int x, int y, DIRECTION direction) {
+            this.x = x;
+            this.y = y;
+            this.direction = direction;
+        }
+
+        public int getX() {
+            return x;
+        }
+
+        public void setX(int x) {
+            this.x = x;
+        }
+
+        public int getY() {
+            return y;
+        }
+
+        public void setY(int y) {
+            this.y = y;
+        }
+
+        public DIRECTION getDirection() {
+            return direction;
+        }
+
+        public void setDirection(DIRECTION direction) {
+            this.direction = direction;
+        }
+    }
+
+    public static void display(int[][] image) {
         for (int x = 0; x < image.length; x++)
         {
             for (int y = 0; y < image[0].length; y++)
@@ -53,7 +91,8 @@ public class CA3_Question9
 
     public static void solve(Player player, int[][] maze, DIRECTION dir) {
         boolean mazeSolved = false;
-        int globalCounter = 0;
+        int globalCounter = 1;
+        Stack<XYDPair> nextInstructionsToFollow = new Stack<>();
 
         System.out.println("Maze Size: " + maze.length);
         System.out.println("Player Starting Position: " + player.getX() + ", " + player.getY());
@@ -64,43 +103,62 @@ public class CA3_Question9
 
         //while player x != 0 || maze.length && player y != 0 || maze.length
         while (!mazeSolved) {
-
-            //check north
-
-
-            //check east
-
-            //check south
-
-            //check west
+            maze[player.getY()][player.getX()] = 999;
 
             display(maze);
             System.out.printf("--= Step %d =--\n", globalCounter);
+
+            //stack adding phase
+            //check north -y,   if 0, add to stack
+            if (maze[player.getY() - 1][player.getX()] == 0) {
+                System.out.println("North is traversable, adding to Stack.");
+                nextInstructionsToFollow.push(new XYDPair(player.getX(), player.getY() - 1, DIRECTION.NORTH));
+            }
+
+            //check east +x,    if 0, add to stack
+            if (maze[player.getY()][player.getX() + 1] == 0) {
+                System.out.println("East is traversable, adding to Stack.");
+                nextInstructionsToFollow.push(new XYDPair(player.getX() + 1, player.getY(), DIRECTION.EAST));
+            }
+
+            //check south +y,   if 0, add to stack
+            if (maze[player.getY() + 1][player.getX()] == 0) {
+                System.out.println("South is traversable, adding to Stack.");
+                nextInstructionsToFollow.push(new XYDPair(player.getX(), player.getY() + 1, DIRECTION.SOUTH));
+            }
+
+            //check west -x,    if 0, add to stack
+            if (maze[player.getY()][player.getX() - 1] == 0) {
+                System.out.println("West is traversable, adding to Stack.");
+                nextInstructionsToFollow.push(new XYDPair(player.getX() - 1, player.getY(), DIRECTION.WEST));
+            }
+
             globalCounter++;
 
             //this just means the player isn't in an edge position, which would be the exit to the maze
-            if ((player.getX() == 0 || player.getX() == maze.length) || (player.getY() == 0 || player.getY() == maze.length)) {
-                System.out.println("Maze solved.");
+            if ((player.getX() == 0 || player.getX() == maze.length - 1) || (player.getY() == 0 || player.getY() == maze.length - 1)) {
+                System.out.printf("Maze solved in %d steps.\n", globalCounter);
                 mazeSolved = !mazeSolved;
             }
         }
     }
 
     public static boolean isValidPlayer(Player player, int[][] maze) {
-        if (player.getX() < 0 || player.getY() < 0 || player.getX() > maze.length || player.getY() > maze.length) {
+        if (player.getX() < 0 || player.getY() < 0 || player.getX() >= maze.length || player.getY() >= maze.length) {
             System.out.println("Invalid: Player is starting outside of maze...");
             return false;
         }
 
         for (int x = 0; x < maze.length; x++) {
             for (int y = 0; y < maze.length; y++) {
-                if (maze[x][y] != 0) {
+                if (maze[player.getY()][player.getX()] != 0) {
                     System.out.println("Invalid: Player is starting on a maze wall...");
                     return false;
                 }
             }
         }
 
+        System.out.println("GOOD: Player is in valid starting position...");
         return true;
     }
 
